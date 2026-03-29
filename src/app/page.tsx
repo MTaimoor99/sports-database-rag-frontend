be379f25e766f073803f1bd3680f8c25f8bfb371
Taimoor <taimoor992002@gmail.com>
2026-03-29 16:14:46 +0500

    added send button to the textfield

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
-import Image from "next/image";
+import {Send} from 'lucide-react';
 
 export default function Home() {
   return (
     <>
     <div className="flex flex-col justify-end h-screen pb-16 px-16">
-      <input type="text" placeholder="Ask something..." className="px-4 py-4"/>
+      <div className='relative'>
+      <input type="text" placeholder="Ask something..." className="px-4 py-4 pr-12 w-full"/>
+      <button className='absolute right-3 top-1/2 -translate-y-1/2'>
+      <Send size={18}/>
+      </button>
+      
+      </div>
     </div>
     </>
   );
